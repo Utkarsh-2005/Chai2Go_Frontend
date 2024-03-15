eaf9056d93bf912902f0fd4e7a857a5f4dd47617
Utkarsh <utkarshjha.4009@gmail.com>
2024-03-15 14:59:32 +0530

    Another routing solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
-import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+  createRoutesFromElements,
+} from "react-router-dom";
 
 import Register from './components/pages/Register';
 import Login from './components/pages/Login';
@@ -6,18 +11,23 @@ import Dashboard from './components/pages/Dashboard'
 import LandingPage from './components/pages/LandingPage';
 import Admin from "./components/pages/Admin"
 
-
-const App = () => {
-  return (
-    <Router>
-    <Routes>
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
     <Route path='/' element={<LandingPage/>}/>
     <Route path='/admin' element={<Admin/>}/>
     <Route path='/register' element={<Register/>}/>
     <Route path='/home/:id' element={<Dashboard/>}/>
     <Route path='/login' element={<Login/>}/>
-  </Routes>
-  </Router>
+  </Route>
+  )
+);
+
+const App = () => {
+  return (
+    <>
+         <RouterProvider router={router} />
+    </>
   );
 };
 
