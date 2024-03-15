738c81253035c2d0ee536af4a669087e01512e37
Utkarsh <utkarshjha.4009@gmail.com>
2024-03-15 19:51:51 +0530

    Added the base directory to the base url of deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,4 @@
-import {
-  createBrowserRouter,
-  Route,
-  RouterProvider,
-  createRoutesFromElements,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Register from './components/pages/Register';
 import Login from './components/pages/Login';
@@ -11,23 +6,17 @@ import Dashboard from './components/pages/Dashboard'
 import LandingPage from './components/pages/LandingPage';
 import Admin from "./components/pages/Admin"
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route>
-    <Route path='/' element={<LandingPage/>}/>
-    <Route path='/admin' element={<Admin/>}/>
-    <Route path='/register' element={<Register/>}/>
-    <Route path='/home/:id' element={<Dashboard/>}/>
-    <Route path='/login' element={<Login/>}/>
-  </Route>
-  )
-);
-
 const App = () => {
   return (
-    <>
-         <RouterProvider router={router} />
-    </>
+    <Router basename="https://chai2-go-frontend.vercel.app/">
+      <Routes>
+        <Route path='/' element={<LandingPage/>}/>
+        <Route path='/admin' element={<Admin/>}/>
+        <Route path='/register' element={<Register/>}/>
+        <Route path='/home/:id' element={<Dashboard/>}/>
+        <Route path='/login' element={<Login/>}/>
+      </Routes>
+    </Router>
   );
 };
 
